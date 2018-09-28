6b1c565bc6fa48170c32d452b7b12047e15e012a
MARINE\Thomas.Kitch <thomas.kitch@navico.com>
2018-09-28 15:55:53 +1200

    loaded patterns

--- src/app/shape-sorter/diamond-palette.jsx
+++ src/app/shape-sorter/diamond-palette.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 
 class DiamondPallete extends React.Component {
+    palletClicked = () =>{
+        this.props.onClick(this.props.id)
+    }
     render() {
         return (
-            <div className='diamond-holder diamond-holder-pallete'>
-                <div className={this.props.pattern[0] === 1 ?"diamond diamond-red" :"diamond"} >
+            <div className='diamond-holder diamond-holder-pallete' onClick={this.palletClicked}>
+                <div className={this.props.pattern[0] !=0 ?"diamond " + this.props.pattern[0] :"diamond"} >
                 </div>
-                <div className={this.props.pattern[1] === 1 ?"diamond diamond-blue" :"diamond"} >
+                <div className={this.props.pattern[1] !=0 ?"diamond " + this.props.pattern[1] :"diamond"} >
                 </div>
-                <div className={this.props.pattern[2] === 1 ? "diamond diamond-green" :"diamond"} >
+                <div className={this.props.pattern[2] !=0 ?"diamond " + this.props.pattern[2] :"diamond"} >
                 </div>
-                <div className={this.props.pattern[3] === 1 ? "diamond diamond-black" :"diamond"} >
+                <div className={this.props.pattern[3] !=0 ?"diamond " + this.props.pattern[3]:"diamond"} >
                 </div>
             </div>
         );
--- src/app/shape-sorter/simple-sorter.jsx
+++ src/app/shape-sorter/simple-sorter.jsx
@@ -11,12 +11,31 @@ import '../content/css/carousel.css';
 class SimpleSorter extends React.Component {
     state = {
         animateDiamondClass: "",
-        patterns: [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
         selectedPatterns:[],
-        testSingles:[[r,0,0,0],[0,g,0,0],[0,0,b,0],[0,0,0,bl]],
-        testOppositesVertical:[[r,0,0,bl],[bl,0,0,r][g,0,0,b][b,0,0,g]],
-        testOppositesHorizontal:[[0,b,g,0],[0,g,b,0],[0,r,bl,0],[0,bl,r,0]],
-        
+        sorterFilledPatterns:[0,0,0,0],
+        challangePatternsPhase1:[],
+        singles:[["red",0,0,0],[0,"green",0,0],[0,0,"blue",0],[0,0,0,"black"]],
+        vertical:[["red",0,0,"black"],["black",0,0,"red"],["green",0,0,"blue"],["blue",0,0,"green"]],
+        horizontal:[[0,"blue","green",0],[0,"green","blue",0],[0,"red","black",0],[0,"black","red",0]],
+        diagonals:[["red","blue",0,0],["red",0,"blue",0],[0,0,"blue","red"],[0,"red",0,"blue"],
+        ["blue","black",0,0],["black",0,"blue",0],[0,0,"black","blue"],[0,"blue",0,"black"],
+        ["green","black",0,0],["green",0,"black",0],[0,0,"black","green"],[0,"black",0,"green"],
+        ["red","green",0,0],["green",0,"red",0],[0,0,"green","red"],[0,"red",0,"green"]],
+        triangles:[["red","green","blue",0],["green",0,"red","blue"],[0,"blue","green","red"],["blue","red","green",0],
+        [0,"green","blue","black"],["green","black",0,"blue"],["black","blue","green",0],["blue",0,"black","green"],
+        ["red","green",0,"black"],["green","black","red",0],["black",0,"green","red"],[0,"red","black","green"],
+        ["red",0,"blue","black"],[0,"black","red","blue"],["black","blue",0,"red"],["blue","red","black",0]]
+    }
+    componentWillMount = () =>{
+        // Perform the set up for the challenge here
+
+        let challangePatternsPhase1 = this.state.singles.map((pattern,index) =>{
+          return {id: index += "singles",pattern:pattern}
+        }
+        );
+
+        this.setState({challangePatternsPhase1});
+
 
     }
     redClicked = () => {
@@ -24,17 +43,43 @@ class SimpleSorter extends React.Component {
             animateDiamondClass: "insert-diamond"
         })
     }
+    patternClicked = (id) =>{
+        debugger;
+      let selectedPattern = this.state.selectedPatterns.find(function (obj) { return obj.id === id; });
+       
+      if(selectedPattern){
+        //   Remove and deselct
+      } else{
+        let pattern = this.state.challangePatternsPhase1.find(function (obj) { return obj.id === id; });
+        //check if fits in sorterFilledPatterns
+        //   add and select
+      }
+    //    1 Check if pattern is already selected if so remove pattern form selected and
+    //     sortedFilledPatterns
+        
+    //    2 Check if pattern fit fits in the sorterFilledPatterns
+    //     Add the pattern to selected 
+
+    //    3 check if game is won.diagonals
+
+
+    //    Need a way to keep track of selected patterns
+
+    }
     render() {
-        var patterns = this.state.patterns.map((pattern, index) =>
-            <div>
-                <DiamondPallete pattern={pattern} />
+        let phase1 = this.state.challangePatternsPhase1.map((pattern, index) =>
+            <div key={pattern.id}>
+                <DiamondPallete id={pattern.id} 
+                                onClick={this.patternClicked} 
+                                pattern={pattern.pattern}
+                                selected={pattern.selected} />
             </div>);
         return (
             <div className="full-page-fex-col-wrapper">
                 <div className="fade-wrapper">
                     <div className="fade-left"></div>
                     <DragScroll className="carousel" width={1500}>
-                       {patterns}
+                       {phase1}
                     </DragScroll>
                     <div className="fade-right"></div>
                 </div>

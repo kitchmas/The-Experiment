b9df61fc6154a7699a448bc0fbf77c345261f9a5
kitchmas <tomk22@hotmail.co.uk>
2018-10-24 20:38:13 +1300

    Creating Experiments selection page

--- src/app/components/experiments/experiments.jsx
+++ src/app/components/experiments/experiments.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 
+import Diamond from '../diamond/diamond.jsx';
+
+import '../../content/css/experiment.css';
+
+
 const Experiments = () => {
     return (
-        <h1>Experiments</h1>
+        <div className="content-wrapper">
+            <h1>Experiments</h1>
+            <div className="experiments-wrapper">
+                <div className="experiment">
+                    <Diamond
+                    topDiamondClass="diamond-red"
+                    rightDiamondClass="diamond-green"
+                    leftDiamondClass="diamond-blue"
+                    bottomDiamondClass="diamond-black"
+                    />
+                </div>
+                <div className="experiment">
+                <Diamond
+                    topDiamondClass="diamond-red"
+                    rightDiamondClass="diamond-green"
+                    leftDiamondClass="diamond-blue"
+                    bottomDiamondClass="diamond-black"
+                    />
+                </div>
+                <div className="experiment">
+                <Diamond
+                    topDiamondClass="diamond-red"
+                    rightDiamondClass="diamond-green"
+                    leftDiamondClass="diamond-blue"
+                    bottomDiamondClass="diamond-black"
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 

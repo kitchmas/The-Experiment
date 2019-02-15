6a4d79d4bcc0d1660888b4aa0268038775536f1a
MARINE\Thomas.Kitch <thomas.kitch@navico.com>
2019-02-15 15:58:38 +1300

    added day cycle

--- src/app/components/globes/weather-globe.jsx
+++ src/app/components/globes/weather-globe.jsx
@@ -6,12 +6,21 @@ const WeatherGlobe = () => {
     return (
         <div className="center-page-wrapper">
             <div className="globe">
+            <div className="sun"></div>
+            <div className="moon"></div>
+            <div className="cloud">
+                <div className="cloud-top"></div>
+                <div className="cloud-body"></div>
+                <div className="cloud-bottom"></div>
+            </div>
                 <div className="tree">
                     <div className="tree-top"></div>
                     <div className="tree-body"></div>
                     <div className="tree-trunk"></div>
                 </div>
+                <div className="ground"></div>
             </div>
+          
         </div>
     )
 }
